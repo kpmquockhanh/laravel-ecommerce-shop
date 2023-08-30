f1bbe1154c75983fb091ac879767c09035be82e1
khanhluu <khanhluu@beeketing.net>
2023-08-30 16:40:12 +0700

    update timize

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Resources\ProductCollection;
-use \App\Http\Resources\ProductResource;
+use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
 
 /*
 |--------------------------------------------------------------------------
