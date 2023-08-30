d46a9c7ec6c4332d4e80647a388248e1fd6aba24
khanhluu <khanhluu@beeketing.net>
2023-08-31 00:07:36 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::prefix('/products')->group(function () {
     Route::get('/', function (Request $request) {
         $products = \App\Models\Product::with(['categories']);
