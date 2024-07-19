7dad510f400c55cc6a0f48ed7b4b405a8b43c029
khanhluu <kpmquockhanh@gmail.com>
2024-07-19 20:27:08 +1000

    feat(nginx): Update config docker

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -3,10 +3,10 @@
     <div class="content">
         <div class="row">
             @foreach($labels as $key => $label)
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="col-lg-4 col-md-6 col-sm-6 my-2">
                     <div class="card card-stats">
-                        <div class="card-body pb-2">
-                            <div class="row h-100">
+                        <div class="card-body">
+                            <div class="row no-gutters">
                                 <div class="col-5 col-md-4">
                                     <div class="d-flex icon-big text-center icon-warning h-100">
                                         <i class="nc-icon {{ $icons[$key] }} text-danger"></i>
